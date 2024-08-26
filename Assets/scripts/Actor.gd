4b570503eb1344eb64ce8adc73623f68277f0077
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-08-26 18:46:49 +0200

    vault backup: 2024-08-26 18:46:48

--- Assets/scripts/Actor.gd
+++ Assets/scripts/Actor.gd
@@ -1,4 +1,6 @@
-extends CharacterBody2D
+extends CharacterBody2D # inherites all propperties from "CharacterBody2D"
+class_name Actor # sets the class name property so we can more easily refrence it when we need to 
+# e.g. see line 1 of Player.gd
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,4 +12,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y += gravity * delta
+	if not is_on_floor():
+		velocity += gravity * delta
--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -1,15 +1,13 @@
-extends CharacterBody2D
-
+extends Actor
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
+	## We'll change the button mapping later but we don't currently have
+	## any ui to interfere with ;)
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -18,8 +16,8 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
