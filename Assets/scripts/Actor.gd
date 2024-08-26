549c75fd952b172d46edc6a0f3a42e001cf7585e
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-08-26 10:35:44 +0200

    vault backup: 2024-08-26 10:35:43

---
+++ Assets/scripts/Actor.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
