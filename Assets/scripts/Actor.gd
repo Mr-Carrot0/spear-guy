ae07b226573f52427842a66c2749a8b123d54b1a
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-08-27 10:01:07 +0200

    physics

--- Assets/scripts/Actor.gd
+++ Assets/scripts/Actor.gd
@@ -1,16 +1,12 @@
 extends CharacterBody2D # inherites all propperties from "CharacterBody2D"
-class_name Actor # sets the class name property so we can more easily refrence it when we need to 
+class_name Actor # sets the class name property so we can more easily refrence it when we need to
 # e.g. see line 1 of Player.gd
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not is_on_floor():
-		velocity += gravity * delta
+		velocity.y += gravity * delta
