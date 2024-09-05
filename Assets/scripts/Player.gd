22092fb8b9d6ab29af8825c7b904f0dcea745433
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-09-05 08:15:17 +0200

    up

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -1,28 +1,33 @@
 extends Actor
-const SPEED = 5000.0
-const JUMP_VELOCITY = -130.0
+
+@onready var _tree := $AnimationTree
+
+const SPEED := 5000.0
+const JUMP_VELOCITY := -150.0
 
 var debug = {
-	"main": true
-	, "pos": false
+	"main": 1
+	, "pos": 0
 }
+func _ready():
+	_tree.active = true
+	
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 
-	## We'll change the button mapping later but we don't currently have
-	## any ui to interfere with ;)
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED * delta
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
 	
+	var dir = velocity.normalized()
+	_tree["parameters/on_ground/blend_position"] = abs(dir.x)
+	_tree["parameters/in_air/blend_position"] = dir.y
+
+
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * SPEED * delta if direction else move_toward(velocity.x, 0, SPEED * delta)
+	
+
 	if debug.main:
 		if debug.pos:
 			print(velocity)
