ceca2564df2e3a2f6083428ba980111cb319aa02
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-09-09 09:19:58 +0200

    vault backup: 2024-09-09 09:19:58

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -19,13 +19,23 @@ func _physics_process(delta) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+		_tree["parameters/m/transition_request"] = "air"
+		_tree["parameters/in_air/transition_request"] = "jump"
+
 	var dir = Input.get_axis("move_left", "move_right")
 	velocity.x = dir * SPEED * delta if dir else move_toward(velocity.x, 0, SPEED * delta)
 	
 	dir = velocity.normalized()
-	
-	
+	# animation_tree["parameters/Transition/transition_request"] = "state_2"
+	_tree["parameters/on_ground/transition_request"] = ["idle", "walk"][abs(dir.x)]
 
+
+	if is_on_floor():
+		_tree["parameters/m/transition_request"] = "ground"
+	elif dir.y < 0:
+		_tree["parameters/in_air/transition_request"] = "fall"
+	
+	
 	if debug.main:
 		if debug.pos:
 			print(velocity)
