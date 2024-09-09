28cb52278fb753adcf1fddf6cf1f2a1758e7fe91
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-09-09 08:59:51 +0200

    vault backup: 2024-09-09 08:59:51

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -2,8 +2,6 @@ extends Actor
 
 @onready var _tree := $AnimationTree
 
-# @onready var _tree2 := $AnimationTree2
-
 const SPEED := 5000.0
 const JUMP_VELOCITY := -150.0
 
@@ -20,15 +18,12 @@ func _physics_process(delta) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	var dir = velocity.normalized()
-	_tree["parameters/on_ground/blend_position"] = abs(dir.x)
-	_tree["parameters/in_air/blend_position"] = dir.y
-	# _tree2["parameters/test/blend_position"] = abs(dir.x)
-	# _tree2["parameters/test/1/blend_position"] = dir.y
 
-	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * SPEED * delta if direction else move_toward(velocity.x, 0, SPEED * delta)
+	var dir = Input.get_axis("move_left", "move_right")
+	velocity.x = dir * SPEED * delta if dir else move_toward(velocity.x, 0, SPEED * delta)
+	
+	dir = velocity.normalized()
+	
 	
 
 	if debug.main:
--- gun-guy-notes/Spear Guy Story Outline.md
+++ gun-guy-notes/Spear Guy Story Outline.md
@@ -5,4 +5,4 @@
 # Beginnings
 1. [Raven Of the Night](<chars/Raven Of the Night.md>) kills the [King of Spears](<King of Spears>) with a black knife through the heart at an official ceremony
 2. This incident incident causes a nation-wide ban on non-polearm weaponry as they're the only type that's virtually impossible to hide and 
-3. The Prince ([Spear guy](<chars/Spear guy.  md>)) who's now determined to avenge his father, is granted [The Spear Of Destruction](<items/The Spear Of Destruction.   md>) 
+3. The Prince ([Spear guy](<chars/Spear guy.  md>)) who's now determined to avenge his father, is granted [The Spear Of Destruction](<items/The Spear Of Destruction.   md>) by the Church
