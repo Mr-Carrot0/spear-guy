3a8632d56ab8a2c25140a73c36e0b7b524e5faff
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-09-10 10:26:36 +0200

    vault backup: 2024-09-10 10:26:35

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -1,7 +1,6 @@
 extends Actor
 
 @onready var _tree: AnimationTree = $AnimationTree
-@onready var _ani_player: AnimationPlayer = $AnimationPlayer
 
 var is_flipped = false
 
@@ -33,7 +32,7 @@ func _physics_process(_delta) -> void:
 
 	if x_dir != 0:
 		is_flipped = bool(1 - x_dir)
-	var direction = velocity.normalized()
+	# var direction = velocity.normalized()
 	
 	sprite.flip_h = is_flipped
 	sprite.offset.x = [0, -3][int(is_flipped)]
@@ -49,8 +48,6 @@ func _physics_process(_delta) -> void:
 	
 	_tree["parameters/m/transition_request"] = "ground" if is_on_floor() and !Input.is_action_just_pressed("jump") else "air"
 	
-	if _tree["parameters/spin/request"] == AnimationNodeOneShot.ONE_SHOT_REQUEST_NONE and direction.y > 0 and $Sprite2D.frame_coords==Vector2(7,3):
-		_tree["parameters/spin/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT
 
 	if debug.main:
 		if debug.phy:
