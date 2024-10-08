869aca6868ae478f358cbde7e5e9af512268c595
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-10-08 08:41:25 +0200

    vault backup: 2024-10-08 08:41:25

--- Assets/scripts/Spear.gd
+++ Assets/scripts/Spear.gd
@@ -1,6 +1,10 @@
 extends Weapon
 class_name Spear
 
+@export var sprite_head: Texture2D
+@export var sprite_body: Texture2D
+@export var sprite_butt: Texture2D
+
 func _init():
 	pass
 	# print(health)
--- Assets/scripts/Weapon.gd
+++ Assets/scripts/Weapon.gd
@@ -4,7 +4,7 @@ class_name Weapon
 @export var max_health: float = 100
 @export var health: float = 100
 @export var damage: float = 10
-@export var sprite: Texture2D
+# @export var sprite: Texture2D
 @export var is_indestrucable: bool = false
 
 var status = "pristine" if not is_indestrucable else "indestrucable"
@@ -17,9 +17,9 @@ func _update():
 		status = "damaged"
 
 func hit(target):
-	if target is Weapon and is_indestrucable == false:
+	if target is Weapon and target.is_indestrucable == false:
 		target.health -= damage
-		if target.health <= 0:
+		if target.health < 0:
 			target.status = "destroyed"
 	
 	if target is Actor:
