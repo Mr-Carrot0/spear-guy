96cc713f28c5dced0dbcb40d44be40c8f4a6d566
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-10-21 09:27:53 +0200

    vault backup: 2024-10-21 09:27:52

--- Assets/Scripts/bar.gd
+++ Assets/Scripts/bar.gd
@@ -3,8 +3,12 @@ class_name Bar
 
 @onready var parent = get_parent()
 
+var init_pos = position
+
 func _ready():
 	parent.connect("health_update", changeSize)
 
 func changeSize(size):
-	scale.x = size
+	size = size if size > 0 else 0
+	scale.x = size / 20
+	position.x = (init_pos.x - size/2) / 2
