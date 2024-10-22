436febbdfc8c0dba5be780a8edc539298758abcd
Mr-Carrot0 <anderssonjohan874@gmail.com>
2024-10-22 12:18:07 +0200

    vault backup: 2024-10-22 12:18:07

--- Assets/Scripts/spear-test.gd
+++ Assets/Scripts/spear-test.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 @onready var butt = $butt
+@onready var body = $body
 @onready var head = $head
-@onready var body = $head
+
+var spacing_global = Vector2(5,-5)
+var off = Vector2(1,-1)
+
+var b_children: Array[Node] # Sprite2D
 
 var i = 0
 
 func _ready():
-	print_debug("butt:", butt.position, "\n", "body:", body.position, "\n", "head:", head.position)
+	# print("butt:", butt.position, "\n", "body:", body.position, "\n", "head:", head.position)
+	b_children = body.get_children()
