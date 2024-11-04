26de4d7165cda080596cfcd2daa06c270a305bbd
enkvadrat <vide.jacobsson@gmail.com>
2024-11-04 10:38:03 +0100

    enum is easier to type

--- Assets/Scripts/Weapon.gd
+++ Assets/Scripts/Weapon.gd
@@ -1,26 +1,32 @@
 extends Resource
 class_name Weapon
 
+enum condition {
+	PRISTINE,
+	DESTROYED,
+	DAMAGED
+}
+
 @export var max_health: float = 100
-@export var health: float = 100
+@export var health: float = max_health
 @export var damage: float = 10
 # @export var sprite: Texture2D
 @export var is_indestrucable: bool = false
 
-var status = "pristine" if not is_indestrucable else "indestrucable"
+var status = condition.PRISTINE
 
 func _update():
-	if status == "pristine" and health < max_health:
-		status = "damaged"
+	if status == condition.PRISTINE and health < max_health:
+		status = condition.DAMAGED
 		
 func _process(_delta):
 	_update()
 	
 func hit(target):
-	if target is Weapon and target.is_indestrucable == false:
+	if target is Weapon and not target.is_indestrucable:
 		target.health -= damage
 		if target.health < 0:
-			target.status = "destroyed"
+			target.status = condition.DESTROYED
 	
 	if target is Actor:
 		target.health -= max(damage - target.armour, 1)
