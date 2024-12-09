57716a01103cd2289c5a0e70e07e8a19d891e7ec
enkvadrat <vide.jacobsson@gmail.com>
2024-12-09 07:39:39 +0100

    add more self drawn Assets add flipComp add icicle add oneway platform

---
+++ Scenes/components/flipComp.gd
@@ -0,0 +1,21 @@
+extends Node
+class_name flipComp
+
+@export var old_flip = false
+
+@export var body: CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	if body:
+		set_flip(body.velocity.x < 0)
+
+func set_flip(value: bool):
+	if value != old_flip:
+		flip()
+		old_flip = value
+		
+func flip():
+	body.scale.x = -1
+	
+#set_flip(true)
+#set_flip(false)
