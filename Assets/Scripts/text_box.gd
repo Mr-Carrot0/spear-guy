3a1323495e0836a5a90e90b8cd2daac094a600a4
Johan <anderssonjohan874@gmail.com>
2024-12-10 10:50:04 +0100

    vault backup: 2024-12-10 10:50:04

--- Assets/Scripts/text_box.gd
+++ Assets/Scripts/text_box.gd
@@ -1,43 +1,49 @@
 extends CanvasLayer
 
+@onready var label: RichTextLabel = $ColorRect/Label
 
-@export_multiline var contents:Array[String]
+@export_multiline var contents: Array[String]
 
-@export var typeingspeed:float = 20
-var base_delay:float = 1 / typeingspeed
+@export var typeingspeed: float = 20
+var base_delay: float = 1 / typeingspeed
 
 var current_content: String
 var current_content_index: int
 var time_elapsed: float = 0
 
 func _ready() -> void:
+	#setting label.set_scroll_follow(true)
 	next_content()
 
 func _process(delta: float) -> void:
 	if current_content_index < current_content.length():
 		time_elapsed += delta
-		var current_char = current_content[current_content_index] 
+		var current_char = current_content[current_content_index]
 		var delay = base_delay
 		
 		if current_char == '\n':
 			delay *= 5
 		
 		if time_elapsed > delay:
-			$ColorRect/Label.text += current_char
+			label.text += current_char
 			time_elapsed = 0
 			current_content_index += 1
 			
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("continue"):
-		next_content()
+		if current_content_index < current_content.length():
+			# label.text += current_content.split()
+			pass
+		else:
+			next_content()
 
-func next_content() -> void: 
+func next_content() -> void:
 	if not contents.is_empty():
 		
 		current_content = contents.pop_front()
 		current_content_index = 0
-		$ColorRect/Label.text = ""
+		label.text = ""
 		
 	else:
 		queue_free()
