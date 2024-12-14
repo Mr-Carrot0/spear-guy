b36aa837a1a6ad123111d6b1f245d9c979878c2d
enkvadrat <vide.jacobsson@gmail.com>
2024-12-14 16:28:42 +0100

    changed to richtexlabel (johans branch) press z to skip to the end

--- Assets/Scripts/text_box.gd
+++ Assets/Scripts/text_box.gd
@@ -1,10 +1,11 @@
 extends CanvasLayer
 
+@onready var label: RichTextLabel = $ColorRect/Label
 
-@export_multiline var contents:Array[String]
+@export_multiline var contents: Array[String]
 
-@export var typeingspeed:float = 20
-var base_delay:float = 1 / typeingspeed
+@export var typeingspeed: float = 20
+var base_delay: float = 1 / typeingspeed
 
 var current_content: String
 var current_content_index: int
@@ -16,28 +17,32 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if current_content_index < current_content.length():
 		time_elapsed += delta
-		var current_char = current_content[current_content_index] 
+		var current_char = current_content[current_content_index]
 		var delay = base_delay
 		
 		if current_char == '\n':
 			delay *= 5
 		
 		if time_elapsed > delay:
-			$ColorRect/Label.text += current_char
+			label.text += current_char
 			time_elapsed = 0
 			current_content_index += 1
 			
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("continue"):
-		next_content()
+		if current_content_index < current_content.length():
+			label.text = current_content
+			current_content_index = current_content.length()
+		else:
+			next_content()
 
-func next_content() -> void: 
+func next_content() -> void:
 	if not contents.is_empty():
 		
 		current_content = contents.pop_front()
 		current_content_index = 0
-		$ColorRect/Label.text = ""
+		label.text = ""
 		
 	else:
 		queue_free()
