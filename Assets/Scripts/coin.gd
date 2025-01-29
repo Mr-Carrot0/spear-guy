07f5bc935b8d2460db6380d5ec96db374dfee259
enkvadrat <vide.jacobsson@gmail.com>
2025-01-29 21:59:51 +0100

    add fun parkor add a big coin that basically is bigger

--- Assets/Scripts/coin.gd
+++ Assets/Scripts/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
+@export var score = 300
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		Globals.change_player_score(300)
+		Globals.change_player_score(score)
 		animation_player.play("pickup")
 		animation_player.animation_finished.connect(queue_free)
--- Assets/Scripts/spring.gd
+++ Assets/Scripts/spring.gd
@@ -1,7 +1,9 @@
 extends Node2D
 @export var strength: int = 400
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_jumpad_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.velocity = -(Vector2(0,strength))
+		audio_stream_player_2d.play()
 		
