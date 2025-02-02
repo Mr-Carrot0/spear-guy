fa14f225f261cf2245f83f3e2505db51b4a7b5a2
enkvadrat <vide.jacobsson@gmail.com>
2025-02-02 15:48:26 +0100

    disable the correct thing so you cannot get more than 1 extra life

--- Assets/Scripts/extra_life.gd
+++ Assets/Scripts/extra_life.gd
@@ -2,16 +2,16 @@ extends Area2D
 @onready var heart: Sprite2D = $Heart
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
 
 func _ready() -> void:
 	body_entered.connect(give_player_life)
 
 func give_player_life(_body: Node2D) -> void:
-	monitoring = false
 	
 	Globals.change_player_extra_life(1)
 
 	animation_player.animation_finished.connect(func(_name: String): queue_free())
 	audio_stream_player_2d.play()
 	animation_player.play("collect")
-	
