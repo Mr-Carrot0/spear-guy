5a1a9616ab8c647ffa07bb8a869abfb0f87b9293
enkvadrat <vide.jacobsson@gmail.com>
2025-02-05 22:13:35 +0100

    remove "debug code" (sorry johan) I belive it is so much simple to comment the print statements out

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -8,19 +8,6 @@ var jump_time = 0.0
 const max_jump_time = 0.3 # maximum time holding the jump button has effect
 const jump_accel = 500.0
 
-var debug = {
-	"main": 0
-	,
-	"phy": 0
-	, "pos": 0
-	, "vel": 1
-	, "dir": 1,
-	"ani": 1
-	, "oneshot": 1
-	,
-}
-
-
 func _ready():
 	_tree.active = true
 	
@@ -55,35 +42,21 @@ func _physics_process(delta) -> void:
 	if not Input.is_action_pressed("jump") and not is_on_floor():
 		jump_time = max_jump_time
 	
-	
 	var x_dir = Input.get_axis("move_left", "move_right")
 	if x_dir:
 		velocity.x = x_dir * speed 
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
-	# var direction = velocity.normalized()
-
-	
 	## TREE
 	# animation_tree["parameters/[Transition]/transition_request"] = "[state]"
 	_tree["parameters/on_ground/transition_request"] = ["idle", "walk"][abs(x_dir)]
 		
 	_tree["parameters/m/transition_request"] = "ground" if is_on_floor() and !Input.is_action_just_pressed("jump") else "air"
 	
-
-	if debug.main:
-		if debug.phy:
-			if debug.pos:
-				print("pos: ", position)
-			if debug.vel:
-				print("vel: ", velocity)
-			if debug.dir:
-				print("x_dir: ", x_dir)
-		if debug.ani:
-			if debug.oneshot:
-				print(AnimationNodeOneShot[_tree["parameters/spin/request"]])
-	
+	# debug...
+	#print("pos: ", position)
+	#print("vel: ", velocity)
+	#print("x_dir: ", x_dir)
+	#print(AnimationNodeOneShot[_tree["parameters/spin/request"]])
 	move_and_slide()
-	
-	
