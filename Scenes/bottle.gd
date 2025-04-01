8d6cc5dab72abfdd0d89c8f74116f7788d92ec2b
enkvadrat <vide.jacobsson@gmail.com>
2025-04-01 15:42:56 +0200

    fix bottle collision

--- Scenes/bottle.gd
+++ Scenes/bottle.gd
@@ -1,10 +1,8 @@
 extends RigidBody2D
 
-#func _ready():
-	#rotation = deg_to_rad(randf_range(-10, 10)) 
-
+const strength = 4
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Actor:  
 		var direction = (global_position.x - body.global_position.x)
 		direction = direction / abs(direction)  
-		apply_central_impulse(Vector2(direction * 10, 0)) 
+		apply_central_impulse(Vector2(direction * strength, 0)) 
